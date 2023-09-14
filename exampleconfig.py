8d2d61b663fc5c521ee8d5a416ce8dc4d18cfd4c
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2023-09-14 10:32:49 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,19 +3,19 @@ from sample_config import Config
 
 class Development(Config):
 
-    APP_ID = 16204934
+    APP_ID = 25996320
     
-    API_HASH = "7a25525b8f1299d64b97b4cd0d48915e"
+    API_HASH = "772cefc3a92ed382b6c24adbd0d3ea26 "
 
-    ALIVE_NAME = "SAKRAN"
+    ALIVE_NAME = "T_4IJ"
 
     DB_URI = "mongodb+srv://ANAETFSH5T:ANAETFSH5T@cluster0.igrdyjb.mongodb.net/?retryWrites=true&w=majority"
 
     STRING_SESSION = "BAC4cyhkMaGu6MttjFZ0S-HciIaNgzrE8abEZODW7LtYFxQroOpUsF7PyainrOQ1XMglzCfHi0C8vcA2_cgDeirz6arqsKT8rotwhPxaVH-_ptK4fTPtQTYu3qHzaHtd0ZNTCjfIv3vUZHyLDrI4nqzGOgL6BHf9II_nSyPsF9napT1j8IGaUuDNpDgsLrvDjWdkhxn6OV23-PQfVKzvBpptf0fO7dYV1CqVVHkrmLACSNYTzm1gU4hFUlRF_Htlfbq1y1qWWU_whKliYsDO2AuU5rC23Ia7i_PVH1-1L3ZwpDWA-4nRZB_ASkGOV1h-f9DtIbSC8pwYYXPZdrB8l-HXAAAAAT0Uxp8A"
 
-    TG_BOT_TOKEN = "5826282066:AAEQRmIVsDvier6uTM0Usds3VSztf5RY4Yo"
+    TG_BOT_TOKEN = "6501683163:AAEPCSwwRe4twtPXvjd14URFZtZPymLEwGw"
 
-    PRIVATE_GROUP_BOT_API_ID = -1001845567799
+    PRIVATE_GROUP_BOT_API_ID = -1922099981
 
     COMMAND_HAND_LER = "."
 
